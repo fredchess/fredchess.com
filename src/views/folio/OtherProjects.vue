adfab71b8438958a40c850a9e942f7c776ad4bda
Frederick Essono <fredericktabi30@gmail.com>
2023-09-11 05:05:11 +0100

    Configuring swiper for others projects section

--- src/views/folio/OtherProjects.vue
+++ src/views/folio/OtherProjects.vue
@@ -2,9 +2,36 @@
 import HeaderSection from '../../components/HeaderSection.vue';
 import SingleOtherProject from './components/SingleOtherProject.vue';
 import projects from '../../datas/projects.json';
+import { onMounted } from 'vue';
 // import {Swiper, SwiperSlide} from 'swiper/vue';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-const selectedProjects = projects.slice(3, 6);
+const selectedProjects = projects.slice(3);
+
+onMounted(() => {
+    new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        autoplay: { delay: 2500, disableOnInteraction: false },
+        loop: true,
+        modules: [Navigation, Pagination],
+        pagination: { 
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true
+         },
+         breakpoints: {
+            1000: {
+               slidesPerView: 2
+            }
+         }
+    })
+})
+
 
 </script>
 
@@ -12,19 +39,25 @@ const selectedProjects = projects.slice(3, 6);
     <section class="otherprojects lg:min-h-screen">
         <HeaderSection :title="'Other noteworthy projects'"/>
         <div class="other_project_container flex justify-between">
-            <SingleOtherProject v-for="(project, idx) in selectedProjects" 
-            :key="idx"
-            :id="project.id"
-            :name="project.name"
-            :description="project.description"
-            :type="project.type"
-            :used_technos="project.used_technos"
-            :live_link="project.live_link"
-            :git_link="project.git_link"
-        />
+            <div class="swiper">
+                <div class="swiper-wrapper">
+                    <SingleOtherProject v-for="(project, idx) in selectedProjects" 
+                        :key="idx"
+                        :id="project.id"
+                        :name="project.name"
+                        :description="project.description"
+                        :type="project.type"
+                        :used_technos="project.used_technos"
+                        :live_link="project.live_link"
+                        :git_link="project.git_link"
+                    />
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
         </div>
     </section>
 </template>
 
 <style scoped>
+    
 </style>
