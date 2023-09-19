c1dcdd1c5dc79fad16009e37160791d20cd6693c
Frederick Essono <fredericktabi30@gmail.com>
2023-09-19 14:58:41 +0100

    update footer infos

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,22 +2,37 @@
 </script>
 
 <template>
-  <footer class="footer border-top-gradient p-7">
+  <footer class="footer border-top-gradient p-7 flex flex-col justify-center items-center space-y-5">
     <div class="container flex flex-col sm:flex-row justify-center items-center m-auto sm:space-x-5 space-y-7 sm:space-y-0">
       <div class="socials flex items-center justify-center space-x-5">
         <a href="https://github.com/frede" target="_blank">
-            <font-awesome-icon icon="fab fa-github" class="fa-2x hover:text-important" />
+            <font-awesome-icon icon="fab fa-github" class="fa-xl hover:text-important" />
         </a>
         <a href="https://www.linkedin.com/in/frede-chess/" target="_blank">
-            <font-awesome-icon icon="fab fa-linkedin-in" class="fa-2x hover:text-important" />
+            <font-awesome-icon icon="fab fa-linkedin-in" class="fa-xl hover:text-important" />
         </a>
         <a href="https://twitter.com/fredechess" target="_blank">
-            <font-awesome-icon icon="fab fa-twitter" class="fa-2x hover:text-important" />
+            <font-awesome-icon icon="fab fa-twitter" class="fa-xl hover:text-important" />
         </a>
         <a href="https://www.instagram.com/fredechess/" target="_blank">
-            <font-awesome-icon icon="fab fa-instagram" class="fa-2x hover:text-important" />
+            <font-awesome-icon icon="fab fa-instagram" class="fa-xl hover:text-important" />
         </a>
       </div>
+    </div>
+    <div class="phones text-center space-y-3 text-sm sm:text-base">
+      <p>WhatsApp: 
+        <a href="https://wa.me/237694157954" class="link">
+          <span class="ml-3">(+237) 694 15 79 54</span>
+        </a>
+      </p>
+      <p>Phone: 
+        <a href="tel:+237694157954" class="link">
+          <span class="ml-3">(+237) 679 23 50 23</span>
+        </a>
+      </p>
+    </div>
+    <div class="creds text-center text-sm">
+      <p>Designed by <a href="https://www.linkedin.com/in/kuete-cyrille" class="link font-bold mx-1" target="_blank">Kuete Cyrille</a> & coded by <a href="https://github.com/fredchess/portfolio-frontend" class="link font-bold mx-1" target="_blank">Frederick Essono</a></p>
     </div>
   </footer>
 </template>
