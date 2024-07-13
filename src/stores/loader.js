e8ddf88d0dcc52284d22cd2f6db813b20fe6d0b7
Frederick Essono <fredericktabi30@gmail.com>
2024-07-13 05:55:57 +0100

    activate loader

--- src/stores/loader.js
+++ src/stores/loader.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useLoader = defineStore("loader", () => {
-    const isLoading = ref(false);
+    const isLoading = ref(true);
 
     function changeLoading(value) {
         isLoading.value = value;
